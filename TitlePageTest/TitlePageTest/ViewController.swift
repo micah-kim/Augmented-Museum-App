ad98ab9e29418c8284b09ef354c5874be0536092
micah-kim <85715752+micah-kim@users.noreply.github.com>
2021-06-24 08:42:08 +0900

    Add files via upload

---
+++ TitlePageTest/TitlePageTest/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  TitlePageTest
+//
+//  Created by Micah Kim on 6/22/21.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/Empty.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Photos", bundle: Bundle.main) else {
+            print("No image available")
+            return
+        }
+        
+        configuration.trackingImages = trackedImages
+        configuration.maximumNumberOfTrackedImages = 1
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.2)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            let titleScene = SCNScene(named: "art.scnassets/dioramaTest.obj")!
+            let titleNode = titleScene.rootNode.childNodes.first!
+            titleNode.position = SCNVector3Zero
+            titleNode.scale = SCNVector3(x: 0.0015, y: 0.0015, z: 0.0015)
+            titleNode.position.y = 0.00
+            
+            planeNode.addChildNode(titleNode)
+            node.addChildNode(planeNode)
+            
+        }
+        
+        return node
+    }
+}
